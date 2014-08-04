ce5f77675be3cb39ad78b002fd652be2c0926a3e
JoAnn Brooks <joannbrooks036@gmail.com>
2014-08-04 12:13:16 -0500

    Item constructor

---
+++ app/models/item.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function Item(body){
+  console.log(body);
+  this.name =body.name;
+  this.dimensions={};
+  this.dimensions.length = body.dimensions.length*1;
+  this.dimensions.width = body.dimensions.width*1;
+  this.dimensions.height = body.dimensions.height*1;
+  this.weight =parseFloat(body.weight);
+  this.color =body.color;
+  this.quantity =parseInt(body.quantity);
+  this.msrp =parseFloat(body.msrp);
+  this.percentOff =parseInt(body.percentOff);
+}
+
+
+module.exports = Item;
---
+++ test/unit/item.js
@@ -0,0 +1,26 @@
+/* jshint expr:true */
+/* global describe, it */
+
+'use strict';
+
+var expect = require('chai').expect;
+var Item = require('../../app/models/item');
+
+describe('Item', function(){
+  describe('constructor', function(){
+    it('should create a new Item object', function(){
+      var item = new Item ({name:'iPod', dimensions:{length:'3', width:'5', height:'10'}, weight:'0.3', color:'purple', quantity:'12', msrp:'49.99', percentOff:'20'});
+
+      expect(item).to.be.instanceof(Item);
+      expect(item.name).to.equal('iPod');
+      expect(item.dimensions.length).to.equal(3);
+      expect(item.dimensions.width).to.equal(5);
+      expect(item.dimensions.height).to.equal(10);
+      expect(item.weight).to.be.closeTo(0.3, 0.1);
+      expect(item.color).to.equal('purple');
+      expect(item.quantity).to.equal(12);
+      expect(item.msrp).to.equal(49.99);
+      expect(item.percentOff).to.equal(20);
+    });
+  });
+});
